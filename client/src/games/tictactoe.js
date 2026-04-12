a9d8931181712355a611ef7c66fdf602818e0f10
Paule-Iris Djeni <pauleirisdgo@gmail.com>
2026-04-12 13:50:45 +0200

    feat/cleaned js + put in comments what needs to be done in JS (divided into small taskblocks)

--- client/src/games/tictactoe.js
+++ client/src/games/tictactoe.js
@@ -1,3 +1,53 @@
-let playerI ="I" ;
-let playerT ="T" ;
-let currentPlayer = playerI ;
+///Players: parameters related to players    
+    let playerI ="I" ;
+    let playerT ="T" ;
+    let currentPlayer = playerI ;
+
+/// Others: Other parameters will be here ( unless a category is created for them later)
+
+
+
+
+
+
+///Grid: parameters and winning conditions related to each grid size
+
+    ///3x3
+
+    ///4x4
+
+    ///5x5
+
+
+
+    
+/// BOT: the AI playing against the the player will be coded here
+
+
+
+/// Here will be all the fuctions
+    ///here for before the game starts 
+
+
+    /// here for during the game 
+
+
+
+     /// here for ending the game
+          
+        /// Here to check win
+
+        /// Here to tie/draw?
+
+
+     
+
+     /// here for what happens when the game ends
+
+
+     /// Here for the restart button (works everytime, before, during and after the game)
+
+
+
+/// Message: here will be all the messages, i'll probably use a for loop to check each state of the game and print a message accordingly
+
