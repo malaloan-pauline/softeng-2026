b9f181080f41b48e1f920e0727dd9678ae31314c
Sandrya SANDANASSAMY <sandrya.sandanassamy.002@student.uni.lu>
2026-04-14 00:24:25 +0200

    :wqfeat: OneStroke game base structure
    :wq#

---
+++ client/src/games/OneStroke/OneStrokeGame.jsx
@@ -0,0 +1,12 @@
+import './OneStrokeGame.css'
+
+function OneStrokeGame() {
+  return (
+    <div className="game-container">
+      <h1>One Stroke Puzzle</h1>
+      <p>Game coming soon...</p>
+    </div>
+  )
+}
+
+export default OneStrokeGame
