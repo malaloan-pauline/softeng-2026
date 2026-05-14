37949c23fa88194d24c682b9531e6e61bebd876e
yasmine <yasmine.zeraidi.001@student.uni.lu>
2026-05-14 17:00:27 +0200

    fix: added routes for games

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage/HomePage'
-import Hangman from "./games/hangman";
+import Hangman from './games/hangman'
 
 function App() {
   return (
@@ -12,7 +12,10 @@ function App() {
         <Route path="/feedback" element={<div>Students Feedback</div>} />
         <Route path="/leaderboard" element={<div>Leaderboard</div>} />
         <Route path="/games/hangman" element={<Hangman />} />
-        <Route path="*" element={<Navigate to="/games/hangman" replace />} />
+        <Route path="/games/tictactoe" element={<div>TicTacToe</div>} />
+        <Route path="/games/clicker" element={<div>Clicker</div>} />
+        <Route path="/games/onestroke" element={<div>OneStroke</div>} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   )
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,20 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App'
-import HomePage from './pages/HomePage/HomePage'
-import Hangman from "./games/hangman";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/*" element={<App />} />
-        <Route path="/games/hangman" element={<Hangman />} />
-        <Route path="/games" element={<div>Games page</div>} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
+    <StrictMode>
+        <App />
+    </StrictMode>
 )
