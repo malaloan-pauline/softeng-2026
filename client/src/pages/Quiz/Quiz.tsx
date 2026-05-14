914d9326bda84cd219695b4675829cc76ab3632c
Sandrya SANDANASSAMY <sandrya.sandanassamy.002@student.uni.lu>
2026-05-14 15:03:34 +0200

    fix: correct quiz route navigation & feat: add Quiz component

---
+++ client/src/pages/Quiz/Quiz.tsx
@@ -0,0 +1,10 @@
+import './Quiz.css'
+
+export default function Quiz() {
+  return (
+    <div className="quiz-wrapper">
+      <h1>IT Match Quiz</h1>
+      <p>Quiz initialized! Work in progress... </p>
+    </div>
+  )
+}
