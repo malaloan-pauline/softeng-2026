6f6ea37862f0a34b26048d7c6f034a007093a174
Pauline MALALO-AN <pauline.malalo.001@student.uni.lu>
2026-05-15 08:09:35 +0200

    fix: get correct version of main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App'
-import HomePage from './pages/HomePage/HomePage'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/*" element={<App />} />
-        <Route path="/games" element={<div>Games page</div>} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
+    <StrictMode>
+        <App />
+    </StrictMode>
 )
