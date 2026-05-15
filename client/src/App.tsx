4a9b61f301e401b148f062e7d8b425b33ee6b735
Paule-Iris Djeni <0240339308@uni.lu>
2026-05-15 23:37:01 +0100

    feat : Add TicTacToe Game

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,29 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage/HomePage'
-import Hangman from './games/hangman'
-import OneStrokeGame from './games/OneStroke/OneStrokeGame'
-import ClickerGame from './games/clicker/ClickerGame'
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import Hangman from "./games/hangman";
+import OneStrokeGame from "./games/OneStroke/OneStrokeGame";
+import ClickerGame from "./games/clicker/ClickerGame";
+import TicTacToe from "./games/tictactoe/tictactoe";
+import "./App.css";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} /> 
         <Route path="/games" element={<div>Games page</div>} />
         <Route path="/quiz" element={<div>IT Match Quiz</div>} />
         <Route path="/feedback" element={<div>Students Feedback</div>} />
         <Route path="/leaderboard" element={<div>Leaderboard</div>} />
         <Route path="/games/hangman" element={<Hangman />} />
-        <Route path="/games/tictactoe" element={<div>TicTacToe</div>} />
-        <Route path="/games/clicker" element={<ClickerGame />} />
         <Route path="/games/onestroke" element={<OneStrokeGame />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/games/clicker" element={<ClickerGame />} />
+        <Route path="/games/tictactoe" element={<TicTacToe />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
