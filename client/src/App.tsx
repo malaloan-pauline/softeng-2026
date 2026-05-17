729594524796babbe1d2abcfddcf288abffae1a4
Yasmine ZERAIDI <yasmine.zeraidi.001@student.uni.lu>
2026-05-17 17:03:23 +0200

    Revert "feat : Add TicTacToe Game"
    This reverts commit 4a9b61f301e401b148f062e7d8b425b33ee6b735.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,26 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage";
-import Hangman from "./games/hangman";
-import OneStrokeGame from "./games/OneStroke/OneStrokeGame";
-import ClickerGame from "./games/clicker/ClickerGame";
-import TicTacToe from "./games/tictactoe/tictactoe";
-import "./App.css";
-
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage'
+import Hangman from './games/hangman'
+import OneStrokeGame from './games/OneStroke/OneStrokeGame'
+import ClickerGame from './games/clicker/ClickerGame'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} /> 
+        <Route path="/" element={<HomePage />} />
         <Route path="/games" element={<div>Games page</div>} />
         <Route path="/quiz" element={<div>IT Match Quiz</div>} />
         <Route path="/feedback" element={<div>Students Feedback</div>} />
         <Route path="/leaderboard" element={<div>Leaderboard</div>} />
         <Route path="/games/hangman" element={<Hangman />} />
-        <Route path="/games/onestroke" element={<OneStrokeGame />} />
+        <Route path="/games/tictactoe" element={<div>TicTacToe</div>} />
         <Route path="/games/clicker" element={<ClickerGame />} />
-        <Route path="/games/tictactoe" element={<TicTacToe />} />
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/games/onestroke" element={<OneStrokeGame />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
