d60728b1b4e3e73b254a1494d01f8a440a2c6154
yasmine <yasmine.zeraidi.001@student.uni.lu>
2026-05-27 01:33:18 +0200

    removing cors for simplicity

--- server/index.js
+++ server/index.js
@@ -5,22 +5,7 @@ const leaderboardRoutes = require('./routes/leaderboard');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: function(origin, callback) {
-    if (
-      !origin ||
-      origin.startsWith('http://localhost') ||
-      origin.startsWith('http://192.168.') ||
-      origin.startsWith('http://10.') ||
-      origin.includes('vercel.app') ||
-      origin.includes('railway.app')
-    ) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
+app.use(cors());
 
 app.use(express.json());
 
