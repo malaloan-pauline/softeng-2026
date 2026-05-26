684825a418c5b2853135fcd662b66a115548a5e8
yasmine <yasmine.zeraidi.001@student.uni.lu>
2026-05-27 01:36:55 +0200

    Revert "removing cors for simplicity"
    This reverts commit 5907509555d71caef98ec3b700e545cd9ae539a1.

--- server/index.js
+++ server/index.js
@@ -5,7 +5,22 @@ const leaderboardRoutes = require('./routes/leaderboard');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: function(origin, callback) {
+    if (
+      !origin ||
+      origin.startsWith('http://localhost') ||
+      origin.startsWith('http://192.168.') ||
+      origin.startsWith('http://10.') ||
+      origin.includes('vercel.app') ||
+      origin.includes('railway.app')
+    ) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 
 app.use(express.json());
 
